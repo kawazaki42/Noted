7af43b1c9ac8c7cf1e18a6d3fa07225177675a5d
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-09-30 14:58:53 +0900

    updated oop/task1

--- oop/task1/main.cpp
+++ oop/task1/main.cpp
@@ -13,11 +13,20 @@
 // потоки ввода-вывода
 #include <iostream>
 
+// std::string, std::stod
+#include <string>
+
 // вычисления
-#include "calc.h"
+#include "calc.hpp"
 
 // использовать функции из стандартной библиотеки C++
 // using namespace std;
+
+// Отобразить текст справки.
+void help(std::string argv_0) {
+    std::cout << "Usage: " << std::endl;
+    std::cout << argv_0 << " [<x> <y> <z>]" << std::endl;
+}
 
 
 /**
@@ -27,34 +36,59 @@
 int main(int argc, char **argv) {
 
     // argv[0] - имя программы
-    // аргументы идут после
-    if(argc - 1 >= 1) {
+    // аргументы идут после.
+
+    // Если передан 1 аргумент
+    if(argc-1 == 1) {
         // if(std::string("--test") == std::string(argv[1]) ) {
-        if(std::string("--test") == argv[1] ) {
+        const std::string arg_test = "--test";
+        const std::string arg_help = "--help";
+
+        if(arg_test == argv[1] ) {
             calc::test::run();
+
+            // Если test::run не завершил программу - тесты пройдены.
+            return 0;
+        }
+
+        if(arg_help == argv[1] ) {
+            help(argv[0]);
             return 0;
         }
     }
-
+    
     /// Входные данные
     double x, y, z;
 
-    /*
-     * cout - поток вывода
-     * cin - поток ввода
-     * endl - перевод строки
-     */
+    if (argc-1 == 3) {
+        x = std::stod(argv[1]);
+        y = std::stod(argv[2]);
+        z = std::stod(argv[3]);
+    } else if (argc-1 == 0) {
+        /*
+         * cout - поток вывода
+         * cin - поток ввода
+         * endl - перевод строки
+         */
+    
+        std::cout << "Введите x: ";
+        std::cin >> x;
+    
+        std::cout << "Введите y: ";
+        std::cin >> y;
+    
+        std::cout << "Введите z: ";
+        std::cin >> z;
+    
+        std::cout << std::endl;
+    } else {
+        std::cerr << "Неправильное кол-во аргументов!" << std::endl;
 
-    std::cout << "Введите x: ";
-    std::cin >> x;
+        help(argv[0]);
 
-    std::cout << "Введите y: ";
-    std::cin >> y;
+        return 1;
+    }
 
-    std::cout << "Введите z: ";
-    std::cin >> z;
-
-    std::cout << std::endl;
 
     std::cout
         << "a = "
