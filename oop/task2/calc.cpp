04199235bbefa4b65617e00f9d6d159b745b3ce6
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-10-06 22:57:12 +0900

    v1 of task 2

--- oop/task2/calc.cpp
+++ oop/task2/calc.cpp
@@ -10,18 +10,48 @@
  * @see https://ivtipm.github.io/Programming/Glava01/index01.htm#z11
  */
 
+// Заголовочные файлы (_импорты_)
+
+// Стандартная библиотека
+// assert (для тестов)
+#include <cassert>
+
+// sin
 #include <cmath>
 
-// rand, srand
-#include <cstdlib>
-// time
-#include <ctime>
+// ssize_t
+#include <cstddef>
+
+// // rand, srand
+// #include <cstdlib>
+
+// // time
+// #include <ctime>
+
+// setw, setprecision, fixed
+#include <iomanip>
+
+// потоки ввода-вывода (для arr::display)
+#include <iostream>
 
 // Библиотека случайных чисел из C++
 #include <random>
 
+// математические константы
+// #include <numbers>
 
+
+// Внутренние зависимости
+
+// интерфейс этого модуля
+#include "calc.hpp"
+
+
+/// Логика программы.
 namespace calc {
+    /// Найти синус модуля суммы последовательности.
+    /// @param nums последовательность (динамический массив)
+    /// @param len длина последовательности
     double sin_abs_sum(double nums[], size_t len) {
         double sum = 0.0;
 
@@ -31,21 +61,108 @@ namespace calc {
 
         return sin( abs(sum) );
     }
+
+    /// Тесты для логики программы.
+    namespace test {
+        /// Сравнить два дробных значения с точностью `calc::test::EPSILON`
+        /// @param a, b дробные значения
+        /// @return true, если значения достаточно близки
+        /// @return false, если значения достаточно далеки
+        inline bool is_close(double a, double b) {
+            return std::abs(a - b) < test::EPSILON;
+        }
+        
+        /// TODO
+        /// запустить один тест
+        inline void one(double nums[], size_t len, double expected) {
+            assert(
+                is_close(
+                    sin_abs_sum(nums, len),
+                    expected
+                )
+            );
+        }
+
+        /// TODO
+        /// запустить все тесты
+        void all() {
+            double set[] = {1, -1, 2, -2};
+            one(set, 4, 0);
+        }
+    }
 }
 
 
+/// Работа с массивами.
 namespace arr {
-    void randomize(double nums[], size_t len) {
-        // Инициализируем генератор случайных чисел.
+    /// Заполнить массив случайными числами.
+    /// @param nums массив типа `double`
+    /// @param len длина массива
+    /// @param min минимальное значение случайного числа
+    /// @param max максимальное значение случайного числа
+    void randomize(
+        double nums[],
+        size_t len,
+        double min,  // default: -100.0
+        double max   // default: +100.0
+    ) {
+        // // Инициализируем генератор случайных чисел.
         // TODO: вынести в main?
         // srand(time(NULL));
 
+        // устройство - источник случайных чисел
         std::random_device rd;
+
+        // "движок" случайных чисел
         std::default_random_engine eng(rd());
-        std::uniform_real_distribution dist(-100.0, +100.0);
+
+        // диапазон случайных чисел
+        std::uniform_real_distribution dist(min, max);
 
         for(size_t i = 0; i < len; i++) {
+            // следующее случайное число
+            // в диапазоне dist
+            // сгенерированное при помощи eng
             nums[i] = dist(eng);
+        }
+    }
+
+    /// Отобразить массив на стандартном выводе.
+    ///
+    /// Группы по 10 чисел разделяются на отдельные строки.
+    /// @param nums массив
+    /// @param len длина массива
+    void display(double nums[], size_t len) {
+        const size_t ELEMS_PER_ROW = 10;
+
+        for(size_t i = 0; i < len; i++) {
+            // операция над потоком вывода:
+            std::cout
+
+                // не использовать формат по типу 6.022e+23
+                // для дробных чисел
+                << std::fixed
+
+                // кол-во цифр после запятой
+                << std::setprecision(2)
+
+                // ширина одного поля (числа)
+                << std::setw(10)
+
+                // само число
+                << nums[i];
+            
+            // делим числа на группы:
+            // 0-9, 10-19, 20-29...
+            // (по 10 чисел в каждой)
+
+            /// Позиция элемента в строке
+            size_t rowpos = i % ELEMS_PER_ROW;
+
+            // Если элемент последний в строке, выполнить перенос
+            if(rowpos == ELEMS_PER_ROW-1) {
+                std::cout << std::endl;
+            }
         }
     }
 }
---
+++ oop/task2/calc.hpp
@@ -0,0 +1,65 @@
+/**
+ * @author kawazaki42
+ * 
+ * Логика программы (интерфейсная часть)
+ * 
+ * (136)
+ *     Дана последовательность a[1..n]. Вычислить:
+ *     м) sin | a_1 + ... + a_n |
+ * 
+ * @see https://ivtipm.github.io/Programming/Glava01/index01.htm#z11
+ */
+
+// защита от повторных включений: условная директива.
+#ifndef CALC_HPP
+#define CALC_HPP
+
+// size_t
+#include <cstddef>
+
+
+/// Логика программы.
+namespace calc {
+    /// Найти синус модуля суммы последовательности.
+    /// @param nums последовательность (динамический массив)
+    /// @param len длина последовательности
+    double sin_abs_sum(double nums[], size_t len);
+
+    /// Тесты для логики программы.
+    namespace test {
+        /// Точность сравнения.
+        const double EPSILON = 1e-6;
+
+        /// Сравнить два дробных значения с точностью `calc::test::EPSILON`
+        /// @param a, b дробные значения
+        /// @return true, если значения достаточно близки
+        /// @return false, если значения достаточно далеки
+        inline bool is_close(double a, double b);
+    }
+}
+
+
+/// Работа с массивами.
+namespace arr {
+    /// Заполнить массив случайными числами.
+    /// @param nums массив типа `double`
+    /// @param len длина массива
+    /// @param min минимальное значение случайного числа
+    /// @param max максимальное значение случайного числа
+    void randomize(
+        double nums[],
+        size_t len,
+        double min = -100.0,
+        double max = +100.0
+    );
+
+    /// Отобразить массив на стандартном выводе.
+    ///
+    /// Группы по 10 чисел разделяются на отдельные строки.
+    /// @param nums массив
+    /// @param len длина массива
+    void display(double nums[], size_t len);
+}
+
+
+#endif  // CALC_HPP
