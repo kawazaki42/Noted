35bfbb6d4639f4bb64864933d96e3c7e955402cd
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-10-15 20:15:49 +0900

    attempt to omit jmp

--- asm/homework/divisible.pas
+++ asm/homework/divisible.pas
@@ -1,12 +1,31 @@
 {$asmmode intel}
 
-// function IsDivisible(a, b: Int32): ByteBool;
-// begin
-//     asm
-//     end;
-// end;
+function IsDivisible(a, b: Int32): ByteBool;
+// label divisible;
+begin
+    asm
+        mov eax, a
 
-label divisible;
+        // mov ebx, b
+        // idiv ebx
+
+        idiv b
+
+        // cmp edx, 0
+
+        mov @result, dl
+
+        // mov c, 42
+    end;
+// divisible:
+
+    WriteLn(IsDivisible);
+
+    IsDivisible := not IsDivisible;
+
+end;
+
+// label divisible;
 
 var
     a, b: Int32;
@@ -19,32 +38,24 @@ begin
     Write('Делитель: ');
     ReadLn(b);
 
-    asm
-        mov eax, a
-
-        // mov ebx, b
-
-        // idiv ebx
-
-        idiv b
-
-        cmp edx, 0
-
-        je divisible
-
-        // mov c, 42
+    if IsDivisible(a, b) then
+    begin
+        WriteLn(a,' делится на ',b);
+        if b = 2 then
+            WriteLn('т.е. ',a,' нечетно');
+    end else
+    begin
+        WriteLn(a,' НЕ делится на ',b);
+        if b = 2 then
+            WriteLn('т.е. ',a,' четно');
     end;
 
     // WriteLn(c);
 
 // not divisible:
-    WriteLn(a,' НЕ делится на ',b);
-    if b = 2 then
-        WriteLn('т.е. ',a,' нечетно');
-    Exit;
+//     Exit;
 
-divisible:
-    WriteLn(a,' делится на ',b);
-    Exit;
+// divisible:
+//     Exit;
 
 end.
