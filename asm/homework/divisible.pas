c676162123424307726cb4a7a09a5ed8cd10ba1b
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-10-15 22:10:47 +0900

    AHHHHHHH TURNS OUT IDIV RAISES INTERRUPT WHILE IMUL DOESNT SO I NEED TO INIT EDX

--- asm/homework/divisible.pas
+++ asm/homework/divisible.pas
@@ -1,62 +1,43 @@
 {$asmmode intel}
-{$goto on}
-// {$mode objfpc}
-// label divisible;
 
 
 function IsDivisible(a, b: Int32): Boolean;
-// assembler;
-// label divisible;
-// var
-    // r: Byte;
+label divisible;
 begin
-    // Exit;
+    // default
+    IsDivisible := False;
 
     asm
         mov eax, a
         cdq
 
-        mov ebx, b
-        idiv ebx
+        // mov ebx, b
+        // idiv ebx
 
         // do it in one step
 
-        // idiv [b]
+        idiv b
 
         cmp edx, 0
-        // je divisible
-
-        mov @result, False
-        // ret
-
-    // divisible:
-        // mov r, 0
-        mov @result, True
+        je divisible
     end;
-    // IsDivisible := False;
-    // Exit(False);
+    Exit;
 
-// 1:
-    // IsDivisible := True;
-    // Exit;
-    // Exit(True);
-
-    // IsDivisible := Boolean(r);
-
-    // WriteLn(r);
+divisible:
+    IsDivisible := True;
+    Exit;
 end;
 
 
 var
     a, b: Int32;
-    // c: Boolean;
 
 begin
     Write('Делимое: ');
-    ReadLn(a);
+    Read(a);
 
     Write('Делитель: ');
-    ReadLn(b);
+    Read(b);
 
     if IsDivisible(a, b) then
     begin
@@ -69,13 +50,4 @@ begin
         if b = 2 then
             WriteLn('т.е. ',a,' нечетно');
     end;
-
-    // WriteLn(c);
-
-// not divisible:
-//     Exit;
-
-// divisible:
-//     Exit;
-
 end.
