84a04a23ad861d40a3721f900326f9840bb1c224
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-11-03 19:48:44 +0900

    separate interface and implementation

--- oop/task2/calc.hpp
+++ oop/task2/calc.hpp
@@ -30,31 +30,12 @@ namespace calc {
     /// @param nums последовательность (динамический массив)
     /// @param len длина последовательности
     template<typename T>
-    T sin_abs_sum(const T nums[], size_t len) {
-        T sum = 0.0;
-
-        // for(T elem : nums) {
-        for(size_t i = 0; i < len; i++) {
-            sum += nums[i];
-        }
-
-        return std::sin( std::abs(sum) );
-    }
+    T sin_abs_sum(const T nums[], size_t len);
 
     /// Найти синус модуля суммы последовательности.
     /// @param nums последовательность (`std::vector`)
     template<typename T>
-    T sin_abs_sum(const std::vector<T> nums) {
-        // T sum = std::accumulate(nums.begin(), nums.end(), 0);
-
-        T sum = 0.0;
-
-        for(T elem : nums) {
-            sum += elem;
-        }
-
-        return std::sin( std::abs(sum) );
-    }
+    T sin_abs_sum(const std::vector<T> nums);
 
     /// Тесты для логики программы.
     namespace test {
@@ -79,6 +60,38 @@ namespace calc {
 }
 
 
+/// Найти синус модуля суммы последовательности.
+/// @param nums последовательность (динамический массив)
+/// @param len длина последовательности
+template<typename T>
+T calc::sin_abs_sum(const T nums[], size_t len) {
+    T sum = 0.0;
+
+    // for(T elem : nums) {
+    for(size_t i = 0; i < len; i++) {
+        sum += nums[i];
+    }
+
+    return std::sin( std::abs(sum) );
+}
+
+
+/// Найти синус модуля суммы последовательности.
+/// @param nums последовательность (`std::vector`)
+template<typename T>
+T calc::sin_abs_sum(const std::vector<T> nums) {
+    // T sum = std::accumulate(nums.begin(), nums.end(), 0);
+
+    T sum = 0.0;
+
+    for(T elem : nums) {
+        sum += elem;
+    }
+
+    return std::sin( std::abs(sum) );
+}
+
+
 /// Работа с массивами.
 namespace arr {
     /// Заполнить массив случайными числами.
@@ -94,27 +107,7 @@ namespace arr {
         size_t len,
         T min = -100.0,
         T max = +100.0
-    ) {
-        // // Инициализируем генератор случайных чисел.
-        // TODO: вынести в main?
-        // srand(time(NULL));
-
-        // устройство-источник случайных чисел
-        std::random_device rd;
-
-        // реализация генератора случайных чисел
-        std::default_random_engine eng(rd());
-
-        // диапазон случайных чисел
-        std::uniform_real_distribution dist(min, max);
-
-        for(size_t i = 0; i < len; i++) {
-            // следующее случайное число
-            // в диапазоне dist
-            // сгенерированное при помощи eng
-            nums[i] = dist(eng);
-        }
-    }
+    );
 
     /// Заполнить массив случайными числами.
     /// @param nums массив (`std::vector`)
@@ -127,44 +120,19 @@ namespace arr {
         std::vector<T> &nums,
         T min = -100.0,
         T max = +100.0
-    ) {
-        // устройство-источник случайных чисел
-        std::random_device rd;
-    
-        // реализация генератора случайных чисел
-        std::default_random_engine eng(rd());
-    
-        // диапазон случайных чисел
-        std::uniform_real_distribution dist(min, max);
-    
-        // для заполнения вектора необходимо брать каждый элемент по ссылке
-        for(T &e : nums) {
-            // следующее случайное число
-            // в диапазоне dist
-            // сгенерированное при помощи eng
-            e = dist(eng);
-        }
-    }
+    );
 
     /// Заполнить массив со стандартного ввода.
     /// @param nums массив (`std::vector`) с заданным размером
     ///             (`std::vector::resize`)
     template<typename T>
-    void fill_from_stdin(std::vector<T> &nums) {
-        for(double &e : nums) {
-            std::cin >> e;
-        }
-    }
+    void fill_from_stdin(std::vector<T> &nums);
 
     /// Заполнить массив со стандартного ввода.
     /// @param nums массив
     /// @param size размер массива
     template<typename T>
-    void fill_from_stdin(T *nums, size_t size) {
-        for(size_t i{0}; i < size; i++) {
-            std::cin >> nums[i];
-        }
-    }
+    void fill_from_stdin(T *nums, size_t size);
 
     /// Отобразить массив на стандартном выводе.
     ///
@@ -173,37 +141,7 @@ namespace arr {
     /// @param elems_per_row кол-во элементов на строке
     ///                      (по умалчанию 10)
     template<typename T>
-    void display(T nums[], size_t len, size_t elems_per_row = 10) {
-        for(size_t i = 0; i < len; i++) {
-            // операция над потоком вывода:
-            std::cout
-
-                // не использовать формат по типу 6.022e+23
-                // для дробных чисел
-                << std::fixed
-
-                // кол-во цифр после запятой
-                << std::setprecision(2)
-
-                // ширина одного поля (числа)
-                << std::setw(10)
-
-                // само число
-                << nums[i];
-            
-            // делим числа на группы:
-            // 0-9, 10-19, 20-29...
-            // (по 10 чисел в каждой)
-
-            /// Позиция элемента в строке
-            size_t rowpos = i % elems_per_row;
-
-            // Если элемент последний в строке, выполнить перенос
-            if(rowpos == elems_per_row-1) {
-                std::cout << std::endl;
-            }
-        }
-    }
+    void display(T nums[], size_t len, size_t elems_per_row = 10);
 
     /// Отобразить массив на стандартном выводе.
     ///
@@ -211,37 +149,7 @@ namespace arr {
     /// @param elems_per_row кол-во элементов на строке
     ///                      (по умалчанию 10)
     template<typename T>
-    void display(const std::vector<T> nums, size_t elems_per_row = 10) {
-        for(size_t i = 0; i < nums.size(); i++) {
-            // операция над потоком вывода:
-            std::cout
-
-                // не использовать формат по типу 6.022e+23
-                // для дробных чисел
-                << std::fixed
-
-                // кол-во цифр после запятой
-                << std::setprecision(2)
-
-                // ширина одного поля (числа)
-                << std::setw(10)
-
-                // само число
-                << nums[i];
-            
-            // делим числа на группы:
-            // 0-9, 10-19, 20-29...
-            // (по 10 чисел в каждой)
-
-            /// Позиция элемента в строке
-            size_t rowpos = i % elems_per_row;
-
-            // Если элемент последний в строке, выполнить перенос
-            if(rowpos == elems_per_row-1) {
-                std::cout << std::endl;
-            }
-        }
-    }
+    void display(const std::vector<T> nums, size_t elems_per_row = 10);
 
     /// Работа с файлами
     namespace file {
@@ -249,71 +157,268 @@ namespace arr {
         /// @param name имя файла
         /// @return вектор со значениями из файла
         template<typename T>
-        std::vector<T> load(const std::string &name) {
-            std::ifstream f(name);
-
-            std::vector<T> result;
-
-            for(T n; f >> n;) {
-                // NOTE: каждый вызов push_back выделяет память наперед.
-                result.push_back(n);
-            }
-
-            // освободить выделенную но не использованную память
-            result.shrink_to_fit();
-
-            return result;
-        }
+        std::vector<T> load(const std::string &name);
 
         /// Загрузить массив из файла.
         /// @param name имя файла
         /// @param[out] size переменная с размером массива
         /// @return массив (`T *`) со значениями из файла
         template<typename T>
-        T *load(const std::string &name, size_t &size) {
-            std::ifstream f(name);
-
-            size = 0;
-            for(T ignored; f >> ignored;)
-                size++;
-
-            T *result = new T[size];
-
-            f.clear();
-            f.seekg(0, std::ios::beg);
-
-            // it - указатель, который мы используем в качестве итератора
-
-            for(T *it = result; it < (result + size); it++)
-                f >> *it;
-
-            return result;
-        }
+        T *load(const std::string &name, size_t &size);
 
         /// Сохранить массив в файл
         /// @param name имя файла
         /// @param nums массив (`std::vector`)
         template<typename T>
-        void dump(const std::string &name, std::vector<T> nums) {
-            std::ofstream f(name);
-
-            for(const auto x : nums) {
-                f << x << std::endl;
-            }
-        }
+        void dump(const std::string &name, std::vector<T> nums);
 
         /// Сохранить массив в файл
         /// @param name имя файла
         /// @param nums массив
         /// @param size размер массива
         template<typename T>
-        void dump(const std::string &name, T *nums, size_t size) {
-            std::ofstream f(name);
+        void dump(const std::string &name, T *nums, size_t size);
+    }
+}
 
-            for(size_t i{0}; i < size; i++) {
-                f << nums[i] << std::endl;
-            }
+
+/// Заполнить массив случайными числами.
+/// @param nums массив
+/// @param len длина массива
+/// @param min минимальное значение случайного числа
+///            (по умолчанию -100)
+/// @param max максимальное значение случайного числа
+///            (по умолчанию +100)
+template<typename T>
+void arr::randomize(
+    T nums[],
+    size_t len,
+    T min,
+    T max
+) {
+    // // Инициализируем генератор случайных чисел.
+    // TODO: вынести в main?
+    // srand(time(NULL));
+
+    // устройство-источник случайных чисел
+    std::random_device rd;
+
+    // реализация генератора случайных чисел
+    std::default_random_engine eng(rd());
+
+    // диапазон случайных чисел
+    std::uniform_real_distribution dist(min, max);
+
+    for(size_t i = 0; i < len; i++) {
+        // следующее случайное число
+        // в диапазоне dist
+        // сгенерированное при помощи eng
+        nums[i] = dist(eng);
+    }
+}
+
+
+/// Заполнить массив случайными числами.
+/// @param nums массив (`std::vector`)
+/// @param min минимальное значение случайного числа
+///            (по умолчанию -100)
+/// @param max максимальное значение случайного числа
+///            (по умолчанию +100)
+template<typename T>
+void arr::randomize(
+    std::vector<T> &nums,
+    T min,
+    T max
+) {
+    // устройство-источник случайных чисел
+    std::random_device rd;
+
+    // реализация генератора случайных чисел
+    std::default_random_engine eng(rd());
+
+    // диапазон случайных чисел
+    std::uniform_real_distribution dist(min, max);
+
+    // для заполнения вектора необходимо брать каждый элемент по ссылке
+    for(T &e : nums) {
+        // следующее случайное число
+        // в диапазоне dist
+        // сгенерированное при помощи eng
+        e = dist(eng);
+    }
+}
+
+
+/// Заполнить массив со стандартного ввода.
+/// @param nums массив (`std::vector`) с заданным размером
+///             (`std::vector::resize`)
+template<typename T>
+void arr::fill_from_stdin(std::vector<T> &nums) {
+    for(double &e : nums) {
+        std::cin >> e;
+    }
+}
+
+/// Заполнить массив со стандартного ввода.
+/// @param nums массив
+/// @param size размер массива
+template<typename T>
+void arr::fill_from_stdin(T *nums, size_t size) {
+    for(size_t i{0}; i < size; i++) {
+        std::cin >> nums[i];
+    }
+}
+
+
+/// Отобразить массив на стандартном выводе.
+///
+/// @param nums массив
+/// @param len длина массива
+/// @param elems_per_row кол-во элементов на строке
+///                      (по умалчанию 10)
+template<typename T>
+void arr::display(T nums[], size_t len, size_t elems_per_row) {
+    for(size_t i = 0; i < len; i++) {
+        // операция над потоком вывода:
+        std::cout
+
+            // не использовать формат по типу 6.022e+23
+            // для дробных чисел
+            << std::fixed
+
+            // кол-во цифр после запятой
+            << std::setprecision(2)
+
+            // ширина одного поля (числа)
+            << std::setw(10)
+
+            // само число
+            << nums[i];
+        
+        // делим числа на группы:
+        // 0-9, 10-19, 20-29...
+        // (по 10 чисел в каждой)
+
+        /// Позиция элемента в строке
+        size_t rowpos = i % elems_per_row;
+
+        // Если элемент последний в строке, выполнить перенос
+        if(rowpos == elems_per_row-1) {
+            std::cout << std::endl;
         }
+    }
+}
+
+
+/// Отобразить массив на стандартном выводе.
+///
+/// @param nums массив (`std::vector`)
+/// @param elems_per_row кол-во элементов на строке
+///                      (по умалчанию 10)
+template<typename T>
+void arr::display(const std::vector<T> nums, size_t elems_per_row) {
+    for(size_t i = 0; i < nums.size(); i++) {
+        // операция над потоком вывода:
+        std::cout
+
+            // не использовать формат по типу 6.022e+23
+            // для дробных чисел
+            << std::fixed
+
+            // кол-во цифр после запятой
+            << std::setprecision(2)
+
+            // ширина одного поля (числа)
+            << std::setw(10)
+
+            // само число
+            << nums[i];
+        
+        // делим числа на группы:
+        // 0-9, 10-19, 20-29...
+        // (по 10 чисел в каждой)
+
+        /// Позиция элемента в строке
+        size_t rowpos = i % elems_per_row;
+
+        // Если элемент последний в строке, выполнить перенос
+        if(rowpos == elems_per_row-1) {
+            std::cout << std::endl;
+        }
+    }
+}
+
+
+/// Загрузить массив из файла.
+/// @param name имя файла
+/// @return вектор со значениями из файла
+template<typename T>
+std::vector<T> arr::file::load(const std::string &name) {
+    std::ifstream f(name);
+
+    std::vector<T> result;
+
+    for(T n; f >> n;) {
+        // NOTE: каждый вызов push_back выделяет память наперед.
+        result.push_back(n);
+    }
+
+    // освободить выделенную но не использованную память
+    result.shrink_to_fit();
+
+    return result;
+}
+
+
+/// Загрузить массив из файла.
+/// @param name имя файла
+/// @param[out] size переменная с размером массива
+/// @return массив (`T *`) со значениями из файла
+template<typename T>
+T *arr::file::load(const std::string &name, size_t &size) {
+    std::ifstream f(name);
+
+    size = 0;
+    for(T ignored; f >> ignored;)
+        size++;
+
+    T *result = new T[size];
+
+    f.clear();
+    f.seekg(0, std::ios::beg);
+
+    // it - указатель, который мы используем в качестве итератора
+
+    for(T *it = result; it < (result + size); it++)
+        f >> *it;
+
+    return result;
+}
+
+
+/// Сохранить массив в файл
+/// @param name имя файла
+/// @param nums массив (`std::vector`)
+template<typename T>
+void arr::file::dump(const std::string &name, std::vector<T> nums) {
+    std::ofstream f(name);
+
+    for(const auto x : nums) {
+        f << x << std::endl;
+    }
+}
+
+
+/// Сохранить массив в файл
+/// @param name имя файла
+/// @param nums массив
+/// @param size размер массива
+template<typename T>
+void arr::file::dump(const std::string &name, T *nums, size_t size) {
+    std::ofstream f(name);
+
+    for(size_t i{0}; i < size; i++) {
+        f << nums[i] << std::endl;
     }
 }
 
