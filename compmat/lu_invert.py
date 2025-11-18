bc354942095684c73dd79e36d80dae16f9ee858e
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-11-18 14:06:35 +0900

    lu_invert - working

---
+++ compmat/lu_invert.py
@@ -0,0 +1,109 @@
+import lu
+
+
+# вариант 4
+
+test_matrix = """
+9 8 1 4 9
+6 5 5 2 9
+5 7 9 9 1
+6 8 2 1 3
+6 8 9 9 2
+""".strip().splitlines()
+
+test_matrix = [map(int, line.split()) for line in test_matrix]
+
+test_matrix = [list(i) for i in test_matrix]
+
+# print(test_matrix)
+
+def invert(orig_mat):
+    l, u = lu.decompose(test_matrix)
+
+    linv = invert_triangle(l, upper=False)
+    # matrix_print( matrix_multiply(l, linv) )
+
+    uinv = invert_triangle(u, upper=True)
+    # matrix_print( matrix_multiply(u, uinv) )
+
+    return matrix_multiply(uinv, linv)
+
+
+def matrix_multiply(a, b):
+    result_rows = len(a)
+    result_cols = len(b[0])
+    result = matrix_filled(result_rows, result_cols, filler=0)
+
+    assert len(a[0]) == len(b)
+    vec_size = len(b)
+
+    for i in range(result_rows):
+        for j in range(result_cols):
+            for m in range(vec_size):
+                result[i][j] += a[i][m] * b[m][j]
+
+    return result
+
+
+def invert_triangle(mat, upper=False):
+    order = len(mat)
+
+    inv = matrix_filled(order, order, filler=0)
+
+    # диагональ
+    for i in range(order):
+        inv[i][i] = 1 / mat[i][i]
+
+    ir = range(order)
+    if upper:
+        ir = reversed(ir)
+
+    for i in ir:
+        jr = range(i)
+        if upper:
+            jr = range(i+1, order)
+            jr = reversed(jr)
+        for j in jr:
+            r = range(j, i)
+            if upper:
+                r = range(i+1, j+1)
+            for m in r:
+                inv[i][j] += mat[i][m] * inv[m][j]
+            inv[i][j] *= -inv[i][i]
+
+    return inv
+
+
+# print(test_matrix)
+
+def matrix_filled(rows, cols, filler=None):
+    result = []
+
+    for i in range(rows):
+        result.append([filler] * cols)
+
+    return result
+
+def matrix_print(mat, end='\n'):
+    for line in mat:
+        line = (round(elem, 2) for elem in line)
+        print(*line)
+        # for elem in line:
+        #     print()
+
+    print(end=end)
+
+# matrix_print(l)
+
+# print()
+
+# matrix_print(u)
+
+if __name__ == '__main__':
+    # matrix_print( matrix_multiply( *lu.decompose(test_matrix) ) )
+    # print()
+    result = invert(test_matrix)
+
+    matrix_print(result)
+
+    matrix_print( matrix_multiply(result, test_matrix) )
