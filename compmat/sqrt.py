03a263551c8e096a8d10411c437b31d2965ee997
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-11-23 15:31:02 +0900

    remove non-procedural code in sqrt.py

--- compmat/sqrt.py
+++ compmat/sqrt.py
@@ -4,7 +4,6 @@
 Вариант 4.
 """
 
-# import copy
 import math
 from matrix import Matrix
 import lu
@@ -17,37 +16,7 @@ test_matrix = Matrix.from_dsv("""
 1 1 6 4 8
 """)
 
-# test_matrix = (
-#     line.split()
-#     for line in test_matrix.splitlines()
-#     if line
-# )
-
-# test_matrix = [list(map(float, row)) for row in test_matrix]
-
-# print(list(test_matrix))
-
 test_column = Matrix.column([24, 35, 38, 46, 37])
-
-# order = len(test_matrix)
-
-# assert len(test_matrix) == len(test_matrix[0]) == len(test_column) == order
-
-# zero_vector = [0] * order
-
-# lower = [zero_vector.copy() for _ in range(order)]
-
-# upper = copy.deepcopy(lower)
-
-# print(lower)
-
-# # левое верхнее значение
-# lower[0][0] = math.sqrt(test_matrix[0][0])
-
-# # Заполняем первый столбец.
-# for i in range(1, order):
-#     lower[i][0] = test_matrix[i][0] / lower[0][0]
-
 
 def decompose(mat_orig):
     assert mat_orig.is_square()
@@ -74,49 +43,6 @@ def decompose(mat_orig):
             lower[i, k] /= lower[k, k]
 
     return lower
-
-
-# print("L:")
-
-# for row in lower:
-#     print(row)
-
-
-# for i in range(order):
-#     for j in range(order):
-#         upper[i][j] = lower[j][i]
-
-
-# print("U:")
-
-# for row in upper:
-#     print(row)
-
-
-# Решаем LY = B, ищем Y
-
-# y = zero_vector.copy()
-
-# for i in range(order):
-#     y[i] = test_column[i]
-#     for j in range(i):
-#         y[i] -= y[j] * lower[i][j]
-#     y[i] /= lower[i][i]
-
-# print("Y:", y)
-
-
-# Решаем L^T X = Y, ищем X
-
-# x = zero_vector.copy()
-
-# for i in reversed(range(order)):
-#     x[i] = y[i]
-#     for j in reversed(range(i+1, order)):
-#         x[i] -= x[j] * upper[i][j]
-#     x[i] /= upper[i][i]
-
-# print("X:", x)
 
 
 def solve(mat, col, debug=False):
@@ -146,6 +72,7 @@ def solve(mat, col, debug=False):
 
 if __name__ == '__main__':
     result = solve(test_matrix, test_column, debug=True)
+
     ax = test_matrix.multiply(result)
 
     print('x =', result)
