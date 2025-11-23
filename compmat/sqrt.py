018a7fb35ba954007acb55a08d8bcc927fb7efe0
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-11-23 15:29:44 +0900

    add sqrt.py to ecosystem

--- compmat/sqrt.py
+++ compmat/sqrt.py
@@ -4,38 +4,40 @@
 Вариант 4.
 """
 
-import copy
+# import copy
 import math
+from matrix import Matrix
+import lu
 
-test_matrix = """
+test_matrix = Matrix.from_dsv("""
 6 4 1 3 1
 4 6 2 6 1
 1 2 7 3 6
 3 6 3 9 4
 1 1 6 4 8
-"""
+""")
 
-test_matrix = (
-    line.split()
-    for line in test_matrix.splitlines()
-    if line
-)
+# test_matrix = (
+#     line.split()
+#     for line in test_matrix.splitlines()
+#     if line
+# )
 
-test_matrix = [list(map(float, row)) for row in test_matrix]
+# test_matrix = [list(map(float, row)) for row in test_matrix]
 
 # print(list(test_matrix))
 
-test_column = [24, 35, 38, 46, 37]
+test_column = Matrix.column([24, 35, 38, 46, 37])
 
-order = len(test_matrix)
+# order = len(test_matrix)
 
-assert len(test_matrix) == len(test_matrix[0]) == len(test_column) == order
+# assert len(test_matrix) == len(test_matrix[0]) == len(test_column) == order
 
-zero_vector = [0] * order
+# zero_vector = [0] * order
 
-lower = [zero_vector.copy() for _ in range(order)]
+# lower = [zero_vector.copy() for _ in range(order)]
 
-upper = copy.deepcopy(lower)
+# upper = copy.deepcopy(lower)
 
 # print(lower)
 
@@ -46,63 +48,106 @@ upper = copy.deepcopy(lower)
 # for i in range(1, order):
 #     lower[i][0] = test_matrix[i][0] / lower[0][0]
 
-# k - номер слоя
-for k in range(order):
 
-    # диагональный элемент
+def decompose(mat_orig):
+    assert mat_orig.is_square()
+    order = mat_orig.nrows
 
-    lower[k][k] = test_matrix[k][k]
-    for j in range(k):
-        lower[k][k] -= lower[k][j] ** 2
-    lower[k][k] = math.sqrt(lower[k][k])
+    lower = Matrix(order, order, filler=0)
 
-    # остальной столбец
+    # k - номер слоя
+    for k in range(order):
 
-    for i in range(k+1, order):
-        lower[i][k] = test_matrix[i][k]
+        # диагональный элемент
+
+        lower[k, k] = mat_orig[k, k]
         for j in range(k):
-            lower[i][k] -= lower[i][j] * lower[k][j]
-        lower[i][k] /= lower[k][k]
+            lower[k, k] -= lower[k, j] ** 2
+        lower[k, k] = math.sqrt(lower[k, k])
+
+        # остальной столбец
+
+        for i in range(k+1, order):
+            lower[i, k] = mat_orig[i, k]
+            for j in range(k):
+                lower[i, k] -= lower[i, j] * lower[k, j]
+            lower[i, k] /= lower[k, k]
+
+    return lower
 
 
-print("L:")
+# print("L:")
 
-for row in lower:
-    print(row)
-
-
-for i in range(order):
-    for j in range(order):
-        upper[i][j] = lower[j][i]
+# for row in lower:
+#     print(row)
 
 
-print("U:")
+# for i in range(order):
+#     for j in range(order):
+#         upper[i][j] = lower[j][i]
 
-for row in upper:
-    print(row)
+
+# print("U:")
+
+# for row in upper:
+#     print(row)
 
 
 # Решаем LY = B, ищем Y
 
-y = zero_vector.copy()
+# y = zero_vector.copy()
 
-for i in range(order):
-    y[i] = test_column[i]
-    for j in range(i):
-        y[i] -= y[j] * lower[i][j]
-    y[i] /= lower[i][i]
+# for i in range(order):
+#     y[i] = test_column[i]
+#     for j in range(i):
+#         y[i] -= y[j] * lower[i][j]
+#     y[i] /= lower[i][i]
 
-print("Y:", y)
+# print("Y:", y)
 
 
 # Решаем L^T X = Y, ищем X
 
-x = zero_vector.copy()
+# x = zero_vector.copy()
 
-for i in reversed(range(order)):
-    x[i] = y[i]
-    for j in reversed(range(i+1, order)):
-        x[i] -= x[j] * upper[i][j]
-    x[i] /= upper[i][i]
+# for i in reversed(range(order)):
+#     x[i] = y[i]
+#     for j in reversed(range(i+1, order)):
+#         x[i] -= x[j] * upper[i][j]
+#     x[i] /= upper[i][i]
 
-print("X:", x)
+# print("X:", x)
+
+
+def solve(mat, col, debug=False):
+    l = decompose(mat)
+    u = l.transpose()
+
+    # Решаем LY = B, ищем Y
+    y = lu.solve_lower(l, col)
+
+    # Решаем L^T X = Y, ищем X
+    x = lu.solve_upper(u, y)
+
+    if debug:
+        print('L:')
+        print(l)
+
+        print('U:')
+        print(u)
+
+        # print('DEBUG:')
+        # print(l.multiply(u) - mat)
+        # print(l.multiply(y) - col)
+        # print(u.multiply(x))
+
+    return x
+
+
+if __name__ == '__main__':
+    result = solve(test_matrix, test_column, debug=True)
+    ax = test_matrix.multiply(result)
+
+    print('x =', result)
+    print('Ax =', ax)
+    print('Ax - b =', ax - test_column)
