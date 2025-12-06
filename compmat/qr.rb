99b9b488b786ab9c350c968756ac50bcfe51a7ea
kawazaki42 <212337579+kawazaki42@users.noreply.github.com>
2025-12-06 21:54:32 +0900

    try to find the source of wrong values

--- compmat/qr.rb
+++ compmat/qr.rb
@@ -1,33 +1,54 @@
 require_relative 'matrix.rb'
 
 module QR
-  TEST_PRIMARY = Adhoc::Matrix.from_space_csv <<~EOF
-    9 8 1 4 9
-    6 5 5 2 9
-    5 7 9 9 1
-    6 8 2 1 3
-    6 8 9 9 2
-  EOF
+  TESTS = [
+    <<~EOF,
+      9 8 1 4 9
+      6 5 5 2 9
+      5 7 9 9 1
+      6 8 2 1 3
+      6 8 9 9 2
+    EOF
 
-  TEST1 = Adhoc::Matrix.from_space_csv <<~EOF
-    2 2 2 1 2
-    2 0 2 1 1
-    1 0 1 1 0
-    1 0 2 2 2
-    1 0 0 2 2
-  EOF
+    <<~EOF,
+      1 6 8
+      2 4 7
+      1 1 3
+    EOF
 
-  TEST2 = Adhoc::Matrix.from_space_csv <<~EOF
-    2 0 0 1 0
-    3 3 1 1 2
-    1 0 0 2 2
-    1 0 0 2 0
-    0 0 1 2 1
-  EOF
+    <<~EOF,
+      2 2 2 1 2
+      2 0 2 1 1
+      1 0 1 1 0
+      1 0 2 2 2
+      1 0 0 2 2
+    EOF
 
+    <<~EOF,
+      2 0 0 1 0
+      3 3 1 1 2
+      1 0 0 2 2
+      1 0 0 2 0
+      0 0 1 2 1
+    EOF
+  ].map{ |str| Adhoc::Matrix.from_space_csv(str, converters: proc { |elem| Rational elem}) }
 
-  TEST_PRIMARY.freeze
-  TEST1.freeze
+  # TEST_PRIMARY = Adhoc::Matrix.from_space_csv <<~EOF
+
+  # TEST1 = Adhoc::Matrix.from_space_csv <<~EOF
+  #   2 2 2 1 2
+  #   2 0 2 1 1
+  #   1 0 1 1 0
+  #   1 0 2 2 2
+  #   1 0 0 2 2
+  # EOF
+
+  # TEST2 = Adhoc::Matrix.from_space_csv 
+
+  # TEST_PRIMARY.freeze
+  # TEST1.freeze
+
+  TESTS.freeze
 
   def self.decompose(matrix)
     fail unless matrix.square?
@@ -81,28 +102,46 @@ module QR
 
     # pp (q * r).round 2
 
-    case id
-    when 0
-      a = TEST_PRIMARY
-    when 1
-      a = TEST1
-    when 2
-      a = TEST2
-    end
+    a = TESTS[id]
+
+    puts "a = "
+    pp a
+
+    # case id
+    # when 0
+    #   a = TEST_PRIMARY
+    # when 1
+    #   a = TEST1
+    # when 2
+    #   a = TEST2
+    # end
 
     q, r = decompose a
     # puts "q * r = "
     # pp q * r
+    
+    puts "q * r - a = "
+    pp q * r - a
 
-    p q * r - a
-
+    id = Adhoc::Matrix.diagonal(a.row_count)
     300.times do
       q, r = decompose a
       # q = q.round 6
       # r = r.round 6
-      pp q.transpose * q - Adhoc::Matrix.diagonal(a.row_count)
       # pp q.transpose * q
+      # pp Adhoc::Matrix.diagonal(a.row_count)
+      # pp q.transpose * q - Adhoc::Matrix.diagonal(a.row_count)
+      # pp q.transpose * q
+      (q*r - a).round(2).each { |elem|
+        fail if not elem.zero?
+      }
+        # .then { pp it }
+      (q * q.transpose - id).round(2).each do |elem|
+        fail if not elem.zero?
+      end
+        # .then {pp it}
       a = r * q
+      a = a.round(6)
     end
 
     pp a.round 2
